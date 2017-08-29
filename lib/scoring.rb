329c4918ac87c7daaed2b4808fe5343e4fc6a5ba
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-08-29 13:54:29 -0700

    Finished self.highest_score method

--- lib/scoring.rb
+++ lib/scoring.rb
@@ -3,15 +3,15 @@ module Scrabble
 
     attr_reader :scores
 
-      SCORES = {
-        1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-        2 => ["D", "G"],
-        3 => ["B", "C", "M", "P"],
-        4 => ["F", "H", "V", "W", "Y"],
-        5 => ["K"],
-        8 => ["J", "X"],
-        10 => ["Q", "Z"]
-      }
+    SCORES = {
+      1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+      2 => ["D", "G"],
+      3 => ["B", "C", "M", "P"],
+      4 => ["F", "H", "V", "W", "Y"],
+      5 => ["K"],
+      8 => ["J", "X"],
+      10 => ["Q", "Z"]
+    }
 
     def self.score(word)
       count = 0
@@ -29,7 +29,60 @@ module Scrabble
 
       return count
 
-    end #end of def self.score
+    end #end of score method
+
+    def self.highest_score(array)
+
+      highest_score = 0
+      highest_scores = []
+      array.each do |word|
+        if self.score(word) > highest_score
+          highest_score = self.score(word)
+          highest_scores = []
+          highest_scores << word
+        elsif self.score(word) == highest_score
+          highest_scores << word
+        end
+      end
+
+      if highest_scores.length == 1
+        return highest_scores[0]
+      end
+
+      #checking for 7 letters
+      # if highest_scores.max_by { |word| word.length } == 7
+      #   seven_letters = []
+      #   highest_scores.each do |word|
+      #     if word.length == 7
+      #       seven_letters << word
+      #     end
+      #   end
+      #   return seven_letters[0]
+      # end
+
+      highest_scores.each do |word|
+        if word.length == 7
+          return word
+        end
+      end
+
+      #checking for fewer letters
+      return highest_scores.min_by { |word| word.length }
+
+      # fewer_letters = []
+      # highest_scores.each do |word|
+      #   if word.length == low
+      #     fewer_letters << word
+      #   end
+      # end
+      # highest_scores.each do |word|
+      #   if word.length == low.length
+      #     return word
+      #   end
+      # end
+      # return fewer_letters[0]
+
+    end #end of highest_score method
 
   end #end of class Scoring
 
