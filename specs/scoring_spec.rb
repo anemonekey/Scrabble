3b8cadf2fa3280e68d5f14ccbe18cdc5ee5aba44
kee nam <nam.keeyeon@gmail.com>
2017-08-28 17:05:39 -0700

    Added tests for highest_score() method and edited lines in wave-x-game.rb files

--- specs/scoring_spec.rb
+++ specs/scoring_spec.rb
@@ -20,38 +20,34 @@ describe "Scoring class" do
     end
   end
 
-  # describe "self.highest_score(array_of_words) method" do
-  #   it "Returns a String" do
-  #     words = ["cat", "boat", "pigeon"]
-  #     winner = Scrabble::Scoring.highest_score(words)
-  #     winner.must_be_kind_of String
-  #   end
-  #
-  #   it "Returns word with the highest score" do
-  #     words = ["cat", "boat", "pigeon"]
-  #     winner = Scrabble::Scoring.highest_score(words)
-  #     winner.must_equal "pigeon"
-  #   end
-  #
-  #   it "When tied, chooses word with fewest letters" do
-  #     # snort, dog, rates == 5 points
-  #     words = ["snort", "dog", "rates"]
-  #     winner = Scrabble::Scoring.highest_score(words)
-  #     winner.must_equal "dog"
-  #   end
-  #
-  #   it "When tied, chooses 7-letter word over word with fewer letters" do
-  #     # terrors = 7 points,
-  #     words = ["terrors"]
-  #     winner = Scrabble::Scoring.highest_score(words)
-  #     winner.must_equal ""
-  #   end
-  #
-  #   it "If many words with same score & length exist, chooses the first one" do
-  #     words = []
-  #     winner = Scrabble::Scoring.highest_score(words)
-  #     winner.must_equal ""
-  #   end
-  # end
+  describe "self.highest_score(array_of_words) method" do
+    it "Returns a String" do
+      words = ["cat", "boat", "pigeon"]
+      Scrabble::Scoring.highest_score(words).must_be_kind_of String
+    end
+
+    it "Returns word with the highest score" do
+      words = ["cat", "boat", "pigeon"]
+      Scrabble::Scoring.highest_score(words).must_equal "pigeon"
+    end
+
+    it "If tied, chooses word with fewest letters" do
+      # snort, dog, rates, need = 5 points
+      words = ["snort", "need", "dog", "rates"]
+      Scrabble::Scoring.highest_score(words).must_equal "dog"
+    end
+
+    it "If tied, chooses 7-letter word over word with fewer letters" do
+      # life, terrors, brain, cob = 7 points
+      words = ["cob", "life", "terrors", "brain"]
+      Scrabble::Scoring.highest_score(words).must_equal "terrors"
+    end
+
+    it "If many words with same score & length, chooses the first" do
+      # heal, hail, vest, hair = 7 points
+      words = ["heal", "hail", "vest", "hair"]
+      Scrabble::Scoring.highest_score(words).must_equal "heal"
+    end
+  end
 
 end
